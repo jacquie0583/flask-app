44b5fe73f0ddb560f80a2abcdfb6b4f399151a86
Jacquelyn Harland <jacquie0583@yahoo.com>
2025-10-05 17:53:25 -0600

    update

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def wish():
     message = "Happy birthday {name}"
     return message.format(name=os.getenv("NAME", "John"))
     
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    
+    
     app.run(host='0.0.0.0', port=8080)
